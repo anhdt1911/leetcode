5248e0d073d5040c70d33dd19b642e47411edcc4
Anh <anhdt1911.work@gmail.com>
2024-03-02 14:02:59 +0700

    valid parentheses https://leetcode.com/problems/valid-parentheses optimize using map

--- ValidParentheses/main.go
+++ ValidParentheses/main.go
@@ -40,3 +40,23 @@ func isValid(s string) bool {
 	}
 	return len(stack) == 1
 }
+
+func isValidV2(s string) bool {
+	brackMap := map[rune]rune{
+		'{': '}',
+		'[': ']',
+		'(': ')',
+	}
+	stack := []rune{}
+	for _, v := range s {
+		if _, ok := brackMap[v]; ok {
+			stack = append(stack, v)
+			// to check if string has none open bracket
+		} else if len(stack) == 0 || brackMap[stack[len(stack)-1]] != v {
+			return false
+		} else {
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
