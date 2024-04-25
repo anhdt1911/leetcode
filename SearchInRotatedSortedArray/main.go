74819bf40e0933ceae087cdd843a524849a11078
Anh <anhdt1911.work@gmail.com>
2024-04-25 14:07:37 +0700

    https://leetcode.com/problems/search-in-rotated-sorted-array/description/

--- SearchInRotatedSortedArray/main.go
+++ SearchInRotatedSortedArray/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(search([]int{4, 5, 6, 7, 8, 0, 1, 2}, 4))
+	fmt.Println(search([]int{4, 5, 6, 7, 8, 0, 1, 2}, 0))
 }
 
 func search(nums []int, target int) int {
@@ -16,5 +16,28 @@ func search(nums []int, target int) int {
 			high = mid
 		}
 	}
-	return nums[low]
+
+	pivotIndex := low
+
+	// not rotated
+	if pivotIndex == 0 {
+		low, high = 0, len(nums)-1
+		// target at right side of pivot
+	} else if pivotIndex != 0 && target < nums[0] {
+		low, high = pivotIndex, len(nums)-1
+	} else {
+		low, high = 0, pivotIndex-1
+	}
+
+	for low <= high {
+		mid := (low + high) / 2
+		if target > nums[mid] {
+			low = mid + 1
+		} else if target < nums[mid] {
+			high = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return -1
 }
