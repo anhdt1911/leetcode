1538a0a720de65c0c13306fd11259bd6bcb1f106
Anh <anhdt1911.work@gmail.com>
2024-07-30 16:48:57 +0700

    longest repeating chracter replacement

--- LongestRepeatingCharacterReplacement/main.go
+++ LongestRepeatingCharacterReplacement/main.go
@@ -3,14 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(characterReplacement("ABAB", 2))
+	fmt.Println(characterReplacement("ABABCD", 2))
 }
 
 func characterReplacement(s string, k int) int {
-	charMap := make(map[rune]int)
-	res := 0
-	for i, v := range s {
-		charMap[v] = i
+	charMap := make(map[byte]int)
+	maxLen := 0
+	var maxRepChar byte = 0
+	start, end := 0, 0
+	for end < len(s) {
+		currentChar := s[end]
+		charMap[currentChar]++
+		count := charMap[currentChar]
+		// check the most repetition character
+		if maxRepChar == 0 || charMap[maxRepChar] < count {
+			maxRepChar = currentChar
+		}
+
+		// keep track of the window size if they exceed max replacement K
+		if end-start+1-charMap[maxRepChar] > k {
+			// reduce char at start index rep number in the map
+			charMap[s[start]]--
+			// decrease window size
+			start++
+		}
+
+		// keep track of max rep
+		if maxLen < end-start+1 {
+			maxLen = end - start + 1
+		}
+		end++
 	}
-	return res
+	return maxLen
 }
---
+++ LongestRepeatingCharacterReplacement/reflect.md
@@ -0,0 +1,7 @@
+Okay so I have to look through the Solutions
+
+Basically you'll need to apply a sliding window
+
+Need to keep track of the maximum size of the window, 
+if the window size minus the maximum character replicate is greater than allowed replacement time provided then you need to reduce the window size
+==> or else the replacement will not be enough to cover all different character in the window for maximum repeating
